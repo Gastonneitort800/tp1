713968217294c3946a4e794257288bf5140998f9
Gastonneitort800 <martingaston558@gmail.com>
2023-05-19 23:36:28 -0300

    se agrega el tp1

---
+++ html/script.js
@@ -0,0 +1,144 @@
+   //  1 //alert("un mensaje");
+  
+  //  2  //document.write("Hello World");
+  
+  //  3 //document.write( 3 + 5 ) ;
+ 
+ 
+  //  4 // let usuario = prompt ("escriba su nombre");
+  //document.write("Hola"+ usuario);
+ 
+ 
+ //  5 //let numero1 = prompt("escribe un numero")
+  //let numero2 = prompt("escribi otro numer")
+  //document.write (parseInt(numero1) + parseInt(numero2));
+
+ 
+ /*  6  let numero1 = prompt("escriba un numero");
+  let numero2 = prompt("escriba otro numero");
+  if (numero1 > numero2) { 
+    document.write ("este es el numero mayor " + numero1);
+  } else {
+    document.write ("este es el numero mayor " + numero2);
+  } */
+
+/* 7
+  let numero1 = prompt("escriba un numero")
+  let numero2 = prompt("escriba un numero")
+  let numero3 = prompt("escriba un numero")
+
+  if (numero1 > numero2 && numero1 > numero3) {
+    document.write ("el " + numero1 + " es el numero mayor");
+  } else if (numero2 > numero3){
+    document.write(
+       "el " + numero2 +" es el mayor" 
+    );
+  }
+   else {
+    document.write( "el " + numero3 + " es el mayor");
+   } */
+
+
+
+    /* 8
+    let num = prompt("Coloque un numero");
+if (num % 2 == 0 ) {
+    document.write("El " + num + " es divisible por 2");
+} else {
+    document.write("El " + num + " no es divisible por 2");
+}  */
+
+
+
+ /* 9
+  
+let frase = prompt("ingresa frase")
+let tamanofrase = frase.length
+let vocales = ""
+
+for(let i=0; i<tamanofrase; i++){
+
+
+  if(frase[i] == "a" || frase[i] == "e" || frase[i] == "i" || frase[i] == "o" || frase[i] == "u")  {
+    vocales = vocales + frase[i];
+  }
+
+}  
+ document.write(vocales);
+ */
+
+
+
+
+
+
+
+ /*  10
+  let num = prompt("Coloque un número");
+
+if (num % 2 == 0) {
+    document.write("El número " + num + " es divisible por 2");
+} else if (num % 3 == 0) {
+    document.write("El número " + num + " es divisible por 3");
+} else if (num % 5 == 0) {
+    document.write("El número " + num + " es divisible por 5");
+} else if (num % 7 == 0) {
+    document.write("El número " + num + " es divisible por 7");
+} else {
+    document.write("El número no se divisible por 2, por 3, por 5 o 7");
+}
+   */
+   
+/*
+let numero = parseInt ( prompt ("ingresar numero"))
+
+if(numero % 2 === 0){
+ 
+  document.write("el numero ingresado " + numero + " es divisible en 2.<br>")
+
+}
+
+if(numero % 3 === 0){
+ 
+  document.write("el numero ingresado " + numero + " es divisible en 3.<br>")
+
+}
+
+if(numero % 5 === 0){
+ 
+  document.write("el numero ingresado " + numero + " es divisible en 5.<br>")
+
+}
+
+if(numero % 7 === 0){
+ 
+  document.write("el numero ingresado " + numero + " es divisible en 7.<br>")
+
+}
+
+if(numero % 2 !== 0 && numero % 3 !== 0 && numero % 5!== 0 && numero % 7 !== 0 ){
+ 
+  document.write("el numero ingresado " + numero + " es divisible en 2, ni en 3, ni en 5, ni en 7..<br>")
+
+}
+
+*/
+
+
+
+
+
+
+
+
+
+    
+
+  
+
+  
+
+  
+
+
+  
